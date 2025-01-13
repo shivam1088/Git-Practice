b3bab902ac7c52a483d8f366bb714062f6211b52
shivam1088 <shivamkgupta135@gmail.com>
2025-01-14 01:05:23 +0530

    modified six.model.py

--- model/six.model.py
+++ model/six.model.py
@@ -59,4 +59,5 @@ if __name__ == "__main__":
 
     # Fetch a user by ID
     fetched = User.get_by_id(1)
-    print(f"Fetched user: {fetched}")
+    s='dfjdshkjf'
+    print(f"Fetched user: {fetched}",{s})
